314980773e1def420c0e8297659cedecfd3d01c7
jotabraga <jpmbraga.feg@gmail.com>
2021-09-06 01:01:55 -0300

    feat: add test config

--- src/config/env.js
+++
@@ -1,7 +0,0 @@
-import path from "path";
-import dotenv from "dotenv";
-
-const { NODE_ENV } = process.env;
-
-const envPath =  NODE_ENV === "test" ? "env.test" : "env";
-dotenv.config({ path: path.resolve(".", envPath) });
--- tests/receitas.test.js
+++ tests/receitas.test.js
@@ -2,7 +2,7 @@ import supertest from 'supertest';
 import app from '../src/app.js';
 
 
-describe("GET /banana", () => {
+describe("GET /transactions", () => {
     it("returns status 200 for valid params", async () => {
         const result = await supertest(app).get("/banana");
         expect(result.status).toEqual(200);
