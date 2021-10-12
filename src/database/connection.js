1a9e61f63c39a8e137adb1a2a295602e6b9f583d
jotabraga <jpmbraga.feg@gmail.com>
2021-10-11 23:35:26 -0300

    feat: add database test to connection in test environment

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,7 @@ import pg from "pg";
 
 const { Pool } = pg;
 const config = {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.NODE_ENV === 'test' ? process.env.DATABASE_TEST_URL : process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     },
